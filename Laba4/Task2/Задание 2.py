d9bc1e8fea0628ceecf4bae022e5f33df99053fc
AntonPatokaProgPy <fear.mega@bk.ru>
2023-10-25 20:07:24 +0300

    Fix

--- Laba4/Task2/Задание 2.py
+++ Laba4/Task2/Задание 2.py
@@ -1,34 +1,38 @@
-def algorithm(n):# 3n
+def algorithm(n):  # 3n
     for i in range(3 * n):
         print(i)
 
-def quick_sort(arr):#nlogn
+
+def quick_sort(arr):  # nlogn
     if len(arr) <= 1:
         return arr
-        pivot = arr[len(arr) // 2]
-        less = [x for x in arr if x < pivot]
-        equal = [x for x in arr if x == pivot]
-        greater = [x for x in arr if x > pivot]
-        return quick_sort(less) + equal + quick_sort(greater)
+    pivot = arr[len(arr) // 2]
+    less = [x for x in arr if x < pivot]
+    equal = [x for x in arr if x == pivot]
+    greater = [x for x in arr if x > pivot]
+    return quick_sort(less) + equal + quick_sort(greater)
 
-def permutations(arr):# n!
+
+def permutations(arr):  # n!
     if len(arr) <= 1:
         return [arr]
     else:
         result = []
         for i in range(len(arr)):
-            rest = arr[:i] + arr[i+1:]
+            rest = arr[:i] + arr[i + 1:]
     for p in permutations(rest):
         result.append([arr[i]] + p)
         return result
 
-def algorithm(n):# n3
+
+def algorithm2(n):  # n3
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 print(i, j, k)
 
-def algorithm(n5):
+
+def algorithm3(n5):
     for i in range(3):
         low = 0
         high = n5 - 1
@@ -38,3 +42,6 @@ def algorithm(n5):
                 high = mid - 1
             else:
                 low = mid + 1
+
+print(quick_sort([1,23,5,6,4]))
+print(permutations([1,23,5,6,4]))
