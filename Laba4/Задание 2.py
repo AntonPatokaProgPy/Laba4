728cbb09e24bf0dee16258744ab6ed9a9bc1f586
AntonPatokaProgPy <fear.mega@bk.ru>
2023-10-25 19:45:20 +0300

    First files

---
+++ Laba4/Задание 2.py
@@ -0,0 +1,40 @@
+def algorithm(n):# 3n
+    for i in range(3 * n):
+        print(i)
+
+def quick_sort(arr):#nlogn
+    if len(arr) <= 1:
+        return arr
+        pivot = arr[len(arr) // 2]
+        less = [x for x in arr if x < pivot]
+        equal = [x for x in arr if x == pivot]
+        greater = [x for x in arr if x > pivot]
+        return quick_sort(less) + equal + quick_sort(greater)
+
+def permutations(arr):# n!
+    if len(arr) <= 1:
+        return [arr]
+    else:
+        result = []
+        for i in range(len(arr)):
+            rest = arr[:i] + arr[i+1:]
+    for p in permutations(rest):
+        result.append([arr[i]] + p)
+        return result
+
+def algorithm(n):# n3
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                print(i, j, k)
+
+def algorithm(n5):
+    for i in range(3):
+        low = 0
+        high = n5 - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if algorithm(n5):
+                high = mid - 1
+            else:
+                low = mid + 1
